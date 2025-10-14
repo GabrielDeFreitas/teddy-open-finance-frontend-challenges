b7fbdce9d0c5fa54e80b40ba464eba4a815a30a7
GabrielDeFreitas <gabrielfreitas.ilustra@gmail.com>
2025-10-14 19:26:48 -0300

    feat: add main routes and navigation structure

--- src/app.tsx
+++
@@ -1,6 +0,0 @@
-import { useTranslation } from 'react-i18next';
-
-export default function App() {
-	const { t } = useTranslation();
-	return <h1 className="text-3xl font-bold underline">{t('header.title')}</h1>;
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './app.tsx';
 import { I18nextProvider } from 'react-i18next';
 import i18n from '../i18n/i18n';
+import { RouteProvider } from './routes.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Root element not found');
@@ -11,7 +11,7 @@ if (!rootElement) throw new Error('Root element not found');
 createRoot(rootElement).render(
 	<StrictMode>
 		<I18nextProvider i18n={i18n}>
-			<App />
+			<RouteProvider />
 		</I18nextProvider>
 	</StrictMode>,
 );
